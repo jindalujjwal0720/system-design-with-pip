0130aa7052086dc70ac8e8cb5f521ae4be9bb987
Ujjwal Jindal <jindalujjwal0720@gmai.com>
2024-09-20 23:30:17 +0530

    Update vite.config.ts to set server port and base path

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000,
+  },
+  base:
+    process.env.NODE_ENV === "production" ? "/system-design-with-pip/" : "/",
 });
